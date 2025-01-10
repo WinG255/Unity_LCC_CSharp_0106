75d75b52f5470e6ab826e68dc730df34abfef02d
WinG255 <twrbwr@gmail.com>
2025-01-10 14:39:31 +0800

    Upd_建立作業

---
+++ Assets/Scripts/Practice_1_Variable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Practice_1_Variable : MonoBehaviour 
+{
+    public uint Price = 999;
+    private float JumpHeight = 77.5f;
+    public string Weapon = "三項之力";
+    private bool DragonSoul = true;
+
+}
---
+++ Assets/Scripts/Practice_2_DataType.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Practice_2_DataType: MonoBehaviour 
+{
+    public float f = 7.7f; //浮點數，4 byte
+    public double db = 0.12345; //倍精浮點數，8 byte
+    public decimal dm = 1.23456789m; //16 byte
+}
