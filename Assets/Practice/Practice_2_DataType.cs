66e14d84fdfe65f9f1bcf055e5ffa5fa7b888e71
WinG255 <twrbwr@gmail.com>
2025-01-13 19:12:58 +0800

    Upd_完成作業練習

--- Assets/Practice/Practice_2_DataType.cs
+++ Assets/Practice/Practice_2_DataType.cs
@@ -4,5 +4,5 @@ public class Practice_2_DataType: MonoBehaviour
 {
     public float f = 7.7f; //浮點數，4 byte
     public double db = 0.12345; //倍精浮點數，8 byte
-    public decimal dm = 1.23456789m; //16 byte
+    public decimal dm = 1.23456789m; //16 byte //Unity不顯示
 }
