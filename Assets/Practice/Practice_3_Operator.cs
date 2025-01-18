2ccf221cc193681a95538e151488167136298494
WinG255 <twrbwr@gmail.com>
2025-01-18 21:17:36 +0800

    Upd_作業(0115)

---
+++ Assets/Practice/Practice_3_Operator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Pracitice_3_Operator : MonoBehaviour 
+{
+    private void Awake()
+    {
+        int num1,num2,num3,num;
+
+        num1 = 6;
+        num2 = 2;
+        num3 = 1;
+
+        num = num1 / num2 * (num3+num2);
+
+        Debug.Log(num);
+    }
+}
---
+++ Assets/Practice/Practice_4_Function.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Practice_4_Function : MonoBehaviour 
+{
+    private void Awake()
+    {
+        bool bTrue, bFalse;
+
+        bTrue = rTrue();
+
+        bFalse = rFalse();
+
+        Debug.Log($"回傳1：{bTrue}，回傳2：{bFalse}");
+    }
+       
+    private bool rTrue()
+    {
+        return true;
+    }
+
+    private bool rFalse()
+    {
+        return false;
+    }
+}
