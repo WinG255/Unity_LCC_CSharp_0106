94060c009e51444ce96a325412369b949531de3e
WinG255 <twrbwr@gmail.com>
2025-02-10 21:41:24 +0800

    Upd_認識屬性

---
+++ Assets/Scripts/Class_7_1_GetSetData.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Properties;
+using UnityEngine;
+
+public class Class_7_1_GetSetData:MonoBehaviour
+{
+    public Class_7_1_Property property;
+
+    private void Awake()
+    {
+        //取得另一個類別的資料
+        Debug.Log(property.moveSpeed);
+        //Debug.Log(property.turnSpeed); //私人無法存取
+
+        //設定另一個類別的資料
+        property.moveSpeed = 7.7f;
+        //property.turnSpeed = 20.2f; //私人無法存取
+
+        Debug.Log(property.runSpeed );
+
+        property.runSpeed = 50.3f;
+        //property.jumpSpeed = 99.5f; //唯讀，不可指定資料
+        //property.jumpSpeed = 99.5f; //唯讀，不可指定資料
+    }
+}
---
+++ Assets/Scripts/Class_7_1_Property.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class Class_7_1_Property:MonoBehaviour
+{
+    //公開變數：外部可讀、寫
+    public float moveSpeed = 3.5f;
+    //私人變數：外部不可讀、寫
+    private float turnSpeed = 12.7f;
+
+    //公開屬性：允許存取
+    public float runSpeed { get; set; }
+
+    //私人：不允許存取
+    private float sprintSpeed {  get; set; }
+
+    //公開：只能讀取(唯讀)
+    public float jumpSpeed { get; }
+
+    //prop + Tab
+    //有預設值的屬性
+    public byte lv { get; set; } = 1;
+
+    private void Awake()
+    {
+        Debug.Log("-----取得變數與屬性-----");
+        Debug .Log (moveSpeed);
+        Debug .Log (turnSpeed);
+        Debug .Log (runSpeed);
+        Debug.Log (sprintSpeed);
+        Debug .Log (jumpSpeed);
+        Debug .Log (lv);
+
+        Debug.Log("-----設定變數與屬性-----");
+        moveSpeed = 11.1f;
+        turnSpeed = 2.5f;
+        runSpeed = 30;
+        sprintSpeed = 60.2f;
+        //jumpSpeed = 90; //沒有set，無法指定值
+        lv = 10;
+
+        
+    }
+
+    //屬性簡寫
+    public float hp { get; set; } = 100;
+
+    //屬性標準寫法
+    //_mp用來裝屬性mp值的容器
+    private float _mp = 50;
+    public float mp
+    {
+        get
+        {
+            return _mp;
+        }
+        set
+        {
+            _mp = value;
+        }
+    }
+
+    //取得屬性的簡寫 (箭頭 => Lambda)
+    //唯讀屬性 防禦 值=30
+    public int defence { get; } = 30;
+    //唯讀屬性 攻擊 值=50
+    public int attack => 50;
+    private void Start()
+    {
+        Debug.Log($"<color=#77f>魔力：{mp}</color>");
+        mp = 500;
+        Debug.Log($"<color=#77f>魔力：{mp}</color>");
+    }
+}
