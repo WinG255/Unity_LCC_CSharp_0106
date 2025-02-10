4f65e3e0219048520ca978c036d6b9593eff89f1
WinG255 <twrbwr@gmail.com>
2025-02-10 19:55:14 +0800

    Upd_認識不規則陣列

--- Assets/Scripts/Class_6_2_Array.cs
+++ Assets/Scripts/Class_6_2_Array.cs
@@ -5,6 +5,7 @@
 /// </summary>
 public class Class_6_2_Array : MonoBehaviour
 {
+    #region 一 ~ 三維陣列
     //不使用陣列的寫法：
     public string card1 = "皮卡丘", card2 = "小火龍", card3 = "卡比獸";
 
@@ -18,22 +19,26 @@ public class Class_6_2_Array : MonoBehaviour
     //定義一種牌組1，可以卡五張卡牌
     public string[] deck1 = new string[5];
     //第二種：直接定義陣列的值
-    public string[] deck2 = { "急凍鳥","賭霞","寶石海星" };
+    public string[] deck2 = { "急凍鳥", "賭霞", "寶石海星" };
 
     //二維陣列
-    public string[,] inventory = { {"紅水","藍水" },{"炸彈","金幣" }};
+    public string[,] inventory = { { "紅水", "藍水" }, { "炸彈", "金幣" } };
     //public string[][] temp = new string[2][] [[" "," " ],[" "," " ]];
 
     //三維陣列
     public string[,,] shop =
     {
-        { 
-            { "小刀", "美工刀" },{ "武士刀", "屠龍刀" } 
-        }, 
-        { 
-            { "精靈球", "高級球" },{ "大師球", "巢穴球" } 
-        } 
+        {
+            { "小刀", "美工刀" },{ "武士刀", "屠龍刀" }
+        },
+        {
+            { "精靈球", "高級球" },{ "大師球", "巢穴球" }
+        }
     };
+
+    private int[][] numbers = new int[2][];
+
+    private int[][,] count = new int[2][,];
 
     private void Awake()
     {
@@ -55,7 +60,7 @@ public class Class_6_2_Array : MonoBehaviour
 
         #region 二維陣列
         //存取二維陣列
-        Debug.Log($"<color=#3f3>編號[0,1]道具：{inventory[0,1]}</color>");
+        Debug.Log($"<color=#3f3>編號[0,1]道具：{inventory[0, 1]}</color>");
 
         inventory[1, 1] = "金創藥";
         Debug.Log($"<color=#3f3>編號[1,1]道具：{inventory[1, 1]}</color>");
@@ -64,7 +69,7 @@ public class Class_6_2_Array : MonoBehaviour
         #region 三維陣列
         //存取三維陣列
         //取得屠龍刀
-        Debug.Log($"<color=#f3d>第一頁第二排第二個：{shop[0,1,1]}</color>");
+        Debug.Log($"<color=#f3d>第一頁第二排第二個：{shop[0, 1, 1]}</color>");
         //設定高級球為起級球
         shop[1, 0, 1] = "超級球";
         Debug.Log($"<color=#f3d>第二頁第一排第二個：{shop[1, 0, 1]}</color>");
@@ -80,5 +85,26 @@ public class Class_6_2_Array : MonoBehaviour
         Debug.Log($"<color=#f93>一維-牌組2的維度：{deck2.Rank}</color>");
         Debug.Log($"<color=#f93>二維-道具的維度：{inventory.Rank}</color>");
         Debug.Log($"<color=#f93>三維-商品的維度：{shop.Rank}</color>");
+    }
+    #endregion
+
+    
+    private void Start()
+    {
+        //不規則陣列：基本練習
+        numbers[0] = new int[] {1,2,3};
+        numbers[1] = new int[] {9,8};
+
+        Debug.Log($"<color=#3ff>不規則陣列的數字 2:{numbers[0][1]}</color>");
+        Debug.Log($"<color=#3ff>不規則陣列的數字 9:{numbers[1][0]}</color>");
+
+        numbers[1][1] = 6;
+        Debug.Log($"<color=#3ff>第二列第二個:{numbers[1][1]}</color>");
+
+        //不規則陣列：包含多維陣列
+        count[0] = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+        count[1] = new int[,] { { 9, 8, 7 }, { 6, 5, 4 } };
+
+        Debug.Log($"<color=#3ff>第二列[0,0]:{count[1][0,0]}</color>");
     }
 }
---
+++ Assets/Scripts/Class_7_property.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Class_7_property:MonoBehaviour
+{
+    //公開變數：外部可讀、寫
+    public float moveSpeed = 3.5f;
+    //私人變數：外部不可讀、寫
+    private float turnSpeed = 12.7f;
+}
