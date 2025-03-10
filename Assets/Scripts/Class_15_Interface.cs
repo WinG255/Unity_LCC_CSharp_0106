32f273ac5cd3c39e3a9c64bc02a5b84d5bcd044c
WinG255 <twrbwr@gmail.com>
2025-03-10 20:57:46 +0800

    upd_添加不使用泛型的對調方法

---
+++ Assets/Scripts/Class_15_Interface.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using WinG.Tools;
+
+public class Class_15_Interface : MonoBehaviour 
+{
+    public object invertoryFirst;
+    public object invertorySecond;
+
+    private void Awake()
+    {
+        int random = Random.Range(0, 3);
+        LogSystem.LogWithColor($"隨機：{random}", "#f33");
+
+        if (random == 0) invertoryFirst = new Prop();
+        else if (random == 1) invertoryFirst = new Equipment1();
+        else if (random == 2) invertoryFirst = new Map();
+
+        int randomSecond = Random.Range(0, 3);
+
+        if (randomSecond == 0) invertorySecond = new Weapon();
+        else if (randomSecond == 1) invertorySecond = new Potion1();
+        else if (randomSecond == 2) invertorySecond = new Chest();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            //如果第一格是道具，就使用道具
+            if (invertoryFirst is Prop) ((Prop)invertoryFirst).Use();
+            //如果第一格是裝備，就使用裝備
+            else if (invertoryFirst is Equipment1) ((Equipment1)invertoryFirst).Use();
+            //如果第一格是地圖，就使用地圖
+            else if (invertoryFirst is Map) ((Map)invertoryFirst).Use();
+
+            //當遊戲的物品種類變多的時候，這邊的判斷式會很恐怖
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            // 如果第二格道具的物品有實作介面，就使用
+            if (invertorySecond is IUse) ((IUse)invertorySecond).Use();
+
+            //如果擴充物品，判斷式不需要添加，只需要讓物品都實作介面即可
+        }
+    }
+}
+
+
+public class Prop
+{
+    public void Use()
+    {
+        LogSystem.LogWithColor("使用道具", "#fa3");
+    }
+}
+
+public class Equipment1
+{
+    public void Use()
+    {
+        LogSystem.LogWithColor("使用裝備", "#fa3");
+    }
+}
+
+public class Map
+{
+    public void Use()
+    {
+        LogSystem.LogWithColor("使用地圖", "#fa3");
+    }
+}
---
+++ Assets/Scripts/Class_15_InterfaceObject.cs
@@ -0,0 +1,58 @@
+﻿using System.Data;
+using UnityEngine;
+using WinG.Tools;
+
+public class Class_15_InterfaceObject : MonoBehaviour
+{
+    
+}
+
+//介面的使用步驟
+//1.定義介面與介面成員
+//2.類別實作介面
+//3.實作介面的成員
+
+//步驟1.
+//帶有「使用」功能的介面，遊戲內可被使用的物品
+public interface IUse
+{
+    //不用宣告方法主體，不用寫大括號
+    public void Use();
+}
+
+public interface IDestroy
+{
+    public void Destroy();
+}
+
+//步驟2.
+//C# 單一繼承，多重實作介面
+public class Weapon : IUse
+{
+    //步驟3.
+    public void Use()
+    {
+        LogSystem.LogWithColor("使用武器，施展武器技能","#fa3");
+    }
+}
+
+public class Potion1 : IUse, IDestroy
+{
+    public void Destroy()
+    {
+        LogSystem.LogWithColor("藥水使用完畢，刪除", "#fa3");
+    }
+
+    public void Use()
+    {
+        LogSystem.LogWithColor("使用藥水，恢復魔力", "#fa3");
+    }
+}
+
+public class Chest : IUse
+{
+    public void Use()
+    {
+        LogSystem.LogWithColor("使用寶箱，獲得隨機道具", "#fa3");
+    }
+}
